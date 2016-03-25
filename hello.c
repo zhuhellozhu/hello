0ef1117d1ea4c427efd652d95d9ef8ce665f0091
Baurzhan Ismagulov <ibr@ilbers.de>
2016-03-25 16:45:29 +0100

    Add hello
    Signed-off-by: Baurzhan Ismagulov <ibr@radix50.net>

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2016 ilbers GmbH
+ *
+ * This program is licensed under the terms of the GNU General Public License
+ * version 2.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, world!\n");
+	return 0;
+}
